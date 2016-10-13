baa1f1f8466f2b99f909d9588ac58cee5d734f82
Parvez Ali <aliparvez20@gmail.com>
2016-10-13 18:28:08 +0530

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from "@angular/core";
 @Component({
     selector: "app",
     templateUrl: "./app/app.html",
-    styleUrls: ['./app/app.component.css']
+    styleUrls: ['./app/app.component.css', './app/footer.component.css']
 })
 export class AppComponent implements OnInit {
     ngOnInit() {
